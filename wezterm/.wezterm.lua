02de7329176ce1ccb54d1d558eab3f043e1f7d29
Jon Nahum <cannahum@outlook.com>
2025-01-05 23:09:58 -0500

    change font-size to 14

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 16
+config.font_size = 14
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
