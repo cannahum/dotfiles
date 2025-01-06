601185356ed60647b41aaff0fe8eb1de4bd6504c
Jon Nahum <cannahum@outlook.com>
2025-01-06 10:27:13 -0500

    update nvim-tree config to be able to programmatically resize tree pane and persist it throughout session

--- nvim/.config/nvim/lua/cannahum/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/cannahum/plugins/nvim-tree.lua
@@ -1,3 +1,33 @@
+local keymap = vim.keymap
+
+local function resize_nvim_tree(amount)
+  local view = require("nvim-tree.view")
+
+  if not view.is_visible() then
+    vim.notify("nvim-tree is not visible", vim.log.levels.INFO)
+    return
+  end
+
+  -- Adjust width dynamically
+  local current_width = view.View.width or 30 -- Default width
+  local new_width = math.max(current_width + amount, 1) -- Ensure minimum width
+  view.View.width = new_width
+  view.resize(new_width)
+
+  -- Persist the width for the session
+  vim.g.nvim_tree_width = new_width
+end
+
+keymap.set("n", "<leader>>", function()
+  resize_nvim_tree(5)
+end, { desc = "Increase nvim-tree width" })
+keymap.set("n", "<leader><", function()
+  resize_nvim_tree(-5)
+end, { desc = "Decrease nvim-tree width" })
+
+-- Restore width on startup
+vim.g.nvim_tree_width = vim.g.nvim_tree_width or 30
+
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = "nvim-tree/nvim-web-devicons",
@@ -10,7 +40,7 @@ return {
 
     nvimtree.setup({
       view = {
-        width = 35,
+        width = vim.g.nvim_tree_width,
         relativenumber = true,
       },
       -- change folder arrow icons
@@ -45,12 +75,9 @@ return {
       },
     })
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }
