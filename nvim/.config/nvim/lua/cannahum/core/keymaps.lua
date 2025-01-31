31299844d18d857c2ee13b5d03643a130186ef9d
Jon Nahum <cannahum@outlook.com>
2025-01-31 15:27:25 -0500

    update vim options and keymaps (inspired by theprimeagen)

--- nvim/.config/nvim/lua/cannahum/core/keymaps.lua
+++ nvim/.config/nvim/lua/cannahum/core/keymaps.lua
@@ -21,3 +21,25 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- -- ThePrimeagen remaps -- --
+-- Block move vertically
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- When appending lower line, cursor remains in its position
+keymap.set("n", "J", "mzJ`z")
+
+-- Set cursor to middle of buffer when moving with C-d and C-u
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Set cursor to middle of buffer when searching and finding next
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+
+-- Pasting over selection will not lose what your were pasting
+keymap.set("x", "<leader>p", '"_dP')
+
+-- Search and replace the current word
+vim.keymap.set("n", "<leader>S", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
