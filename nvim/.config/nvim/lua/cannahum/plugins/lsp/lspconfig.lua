3a3fe6c6d148e4560665bfb0922abbaa15db3941
Jon Nahum <cannahum@outlook.com>
2025-05-27 00:56:57 +0300

    refactor lspconfig and mason, works well, remove diagnostics it's already in core

--- nvim/.config/nvim/lua/cannahum/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/cannahum/plugins/lsp/lspconfig.lua
@@ -8,138 +8,206 @@ return {
   },
   config = function()
     local lspconfig = require("lspconfig")
+    local mason_lspconfig = require("mason-lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local keymap = vim.keymap -- for conciseness
+    vim.filetype.add({ extension = { templ = "templ" } })
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf, silent = true }
+        -- set keybinds
+        opts.desc = "Show LSP references"
+        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+        opts.desc = "Go to declaration"
+        keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+        opts.desc = "Show LSP definitions"
+        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+        opts.desc = "Show LSP implementations"
+        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+        opts.desc = "Show LSP type definitions"
+        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+        opts.desc = "See available code actions"
+        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+        opts.desc = "Smart rename"
+        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+        opts.desc = "Show buffer diagnostics"
+        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+        opts.desc = "Show line diagnostics"
+        keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+        opts.desc = "Go to previous diagnostic"
+        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+        opts.desc = "Go to next diagnostic"
+        keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+        opts.desc = "Show documentation for what is under cursor"
+        keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+        opts.desc = "Restart LSP"
+        keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+      end,
+    })
     local capabilities = cmp_nvim_lsp.default_capabilities()
-    -- Lua
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          runtime = {
-            -- Tell the LSP you're using Neovim's Lua
-            version = "LuaJIT",
-          },
-          diagnostics = {
-            -- Tell the LSP that `vim` is a global variable
-            globals = { "vim" },
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-            checkThirdParty = false,
-          },
-          completion = {
-            callSnippet = "Replace",
-          },
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
-    })
-    -- Go
-    lspconfig.gopls.setup({
-      capabilities = capabilities,
-      on_attach = function(client, bufnr)
-        local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          pattern = "*.go",
-          callback = function()
-            require("go.format").gofmt()
-            require("go.format").goimports()
-          end,
-          group = format_sync_grp,
-        })
-      end,
-    })
-    -- Templ
-    lspconfig.templ.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-    -- Tailwind CSS
-    lspconfig.tailwindcss.setup({
-      capabilities = capabilities,
-      filetypes = { "templ", "astro", "javascript", "typescript", "react", "svelte" },
-      init_options = { userLanguages = { templ = "html" } },
-    })
-    -- HTML
-    lspconfig.html.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "html", "templ" },
-    })
-    -- HTMX
-    lspconfig.htmx.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "html", "templ" },
-    })
-    -- TypeScript
-    lspconfig.ts_ls.setup({
-      capabilities = capabilities,
-      on_attach = function(client, bufnr)
-        -- Add custom keymaps for tsserver, if needed
-        local opts = { buffer = bufnr, silent = true }
-
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-
-        -- Optionally, disable formatting if you prefer to use another tool
-        client.server_capabilities.documentFormattingProvider = false
-      end,
-    })
-    -- Svelte
-    lspconfig.svelte.setup({
-      capabilities = capabilities,
-      on_attach = function(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePost", {
-          pattern = { "*.js", "*.ts" },
-          callback = function(ctx)
-            client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-          end,
-        })
-      end,
-    })
-    -- GraphQL
-    lspconfig.graphql.setup({
-      capabilities = capabilities,
-      filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-    })
-    -- Emmet
-    lspconfig.emmet_ls.setup({
-      capabilities = capabilities,
-      filetypes = {
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "cmake",
+        "cssls",
+        "docker_compose_language_service",
+        "dockerls",
+        "emmet_ls",
+        "gopls",
+        "graphql",
         "html",
-        "typescriptreact",
-        "javascriptreact",
-        "css",
-        "sass",
-        "scss",
-        "less",
+        "htmx",
+        "jsonls",
+        "kotlin_language_server",
+        "lua_ls",
+        "marksman",
+        "omnisharp",
+        "prismals",
+        "pyright",
+        "rust_analyzer",
+        "spectral",
+        "sqlls",
         "svelte",
+        "tailwindcss",
+        "taplo",
+        "templ",
+        "terraformls",
+        "ts_ls",
+        "yamlls",
+        "zls",
       },
-    })
-    -- Kotlin
-    lspconfig.kotlin_language_server.setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "kt", "kts", "kotlin" },
-    })
-    -- Marksman
-    lspconfig.marksman.setup({
-      capabilities = capabilities,
-    })
-    -- Zig
-    lspconfig.zls.setup({
-      capabilities = capabilities,
-      cmd = { vim.fn.stdpath("data") .. "/mason/bin/zls" },
-      on_attach = function(client, bufnr)
-        client.server_capabilities.documentFormattingProvider = true
-        client.server_capabilities.documentRangeFormattingProvider = true
-        vim.diagnostic.config({ virtual_text = true })
-      end,
+      handlers = {
+        -- default handler for installed servers
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+        ["gopls"] = function()
+          lspconfig["gopls"].setup({
+            capabilities = capabilities,
+            on_attach = function(client, bufnr)
+              local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*.go",
+                callback = function()
+                  require("go.format").gofmt() -- gofmt only
+                  require("go.format").goimports() -- goimports + gofmt
+                end,
+                group = format_sync_grp,
+              })
+            end,
+          })
+        end,
+        ["templ"] = function()
+          lspconfig["templ"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+          })
+        end,
+        ["tailwindcss"] = function()
+          lspconfig["tailwindcss"].setup({
+            capabilities = capabilities,
+            filetypes = { "templ", "astro", "javascript", "typescript", "react", "svelte" },
+            init_options = { userLanguages = { templ = "html" } },
+          })
+        end,
+        ["html"] = function()
+          lspconfig["html"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            filetypes = { "html", "templ" },
+          })
+        end,
+        ["htmx"] = function()
+          lspconfig["htmx"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            filetypes = { "html", "templ" },
+          })
+        end,
+        ["svelte"] = function()
+          -- configure svelte server
+          lspconfig["svelte"].setup({
+            capabilities = capabilities,
+            on_attach = function(client, bufnr)
+              vim.api.nvim_create_autocmd("BufWritePost", {
+                pattern = { "*.js", "*.ts" },
+                callback = function(ctx)
+                  -- Here use ctx.match instead of ctx.file
+                  client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+                end,
+              })
+            end,
+          })
+        end,
+        ["graphql"] = function()
+          -- configure graphql language server
+          lspconfig["graphql"].setup({
+            capabilities = capabilities,
+            filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+          })
+        end,
+        ["emmet_ls"] = function()
+          -- configure emmet language server
+          lspconfig["emmet_ls"].setup({
+            capabilities = capabilities,
+            filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+          })
+        end,
+        ["lua_ls"] = function()
+          -- configure lua server (with special settings)
+          lspconfig["lua_ls"].setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                -- make the language server recognize "vim" global
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                completion = {
+                  callSnippet = "Replace",
+                },
+              },
+            },
+          })
+        end,
+        ["kotlin_language_server"] = function()
+          lspconfig["kotlin_language_server"].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            filetypes = { "kt", "kts", "kotlin" },
+          })
+        end,
+        ["marksman"] = function() end,
+        ["zls"] = function()
+          lspconfig["zls"].setup({
+            cmd = { vim.fn.stdpath("data") .. "/mason/bin/zls" },
+            on_attach = function(client, bufnr)
+              client.server_capabilities.documentFormattingProvider = true
+              client.server_capabilities.documentRangeFormattingProvider = true
+              vim.diagnostic.config({ virtual_text = true })
+            end,
+          })
+        end,
+        -- Custom handler for `tsserver`
+        ["typescript-language-server"] = function()
+          lspconfig.tsserver.setup({
+            capabilities = capabilities,
+            on_attach = function(client, bufnr)
+              -- Add custom keymaps for tsserver, if needed
+              local opts = { buffer = bufnr, silent = true }
+              vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+              vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+              vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+              -- Optionally, disable formatting if you prefer to use another tool
+              client.server_capabilities.documentFormattingProvider = false
+            end,
+          })
+        end,
+      },
     })
   end,
 }
--- nvim/.config/nvim/lua/cannahum/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/cannahum/plugins/lsp/mason.lua
@@ -5,14 +5,8 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
-    -- import mason
     local mason = require("mason")
-
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
-
     local mason_tool_installer = require("mason-tool-installer")
-
     -- enable mason and configure icons
     mason.setup({
       ui = {
@@ -21,38 +15,6 @@ return {
           package_pending = "➜",
           package_uninstalled = "✗",
         },
-      },
-    })
-
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "cmake",
-        "cssls",
-        "docker_compose_language_service",
-        "dockerls",
-        "emmet_ls",
-        "gopls",
-        "graphql",
-        "html",
-        "htmx",
-        "jsonls",
-        "kotlin_language_server",
-        "lua_ls",
-        "marksman",
-        "omnisharp",
-        "prismals",
-        "pyright",
-        "rust_analyzer",
-        "spectral",
-        "sqlls",
-        "svelte",
-        "tailwindcss",
-        "taplo",
-        "templ",
-        "terraformls",
-        "ts_ls",
-        "yamlls",
-        "zls",
       },
     })
     mason_tool_installer.setup({
